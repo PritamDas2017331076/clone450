9aa51d9b948ec23be3715a014e360c242af85c57
Zakir <zakirsust2017@gmail.com>
2022-09-24 11:43:33 -0400

    asdf

--- components/ip.js
+++ components/ip.js
@@ -1,3 +1,4 @@
- //export const ip = 'http://192.168.0.106:5000'
- // export const ip = "https://frozen-gorge-43315.herokuapp.com"
- export const ip = "https://arcane-tor-03387.herokuapp.com"
+// export const ip = 'http://192.168.102.212:5000'
+    // export const ip = "https://frozen-gorge-43315.herokuapp.com"
+export const ip = "https://arcane-tor-03387.herokuapp.com"
+    
