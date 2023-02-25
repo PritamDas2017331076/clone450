8eee6876877c39c1339ed2215148f4b46f3438a1
Pritam2017331076 <prishan076@gmail.com>
2023-02-25 16:09:13 +0600

    cganges nothing

--- components/ip.js
+++ components/ip.js
@@ -1,4 +1,5 @@
 //  export const ip = 'http://192.168.102.188:5000'
-// export const ip = 'http://192.168.0.201:5000'
-export const ip = 'https://wide-eyed-ox-bedclothes.cyclic.app'
-    // export const ip = 'https://evening-peak-08449.herokuapp.com'
+// export const ip = 'http://192.168.115.212:5000'
+// export const ip = 'https://wide-eyed-ox-bedclothes.cyclic.app'
+// export const ip = 'https://evening-peak-08449.herokuapp.com'
+export const ip = "https://four50-backend.onrender.com"
