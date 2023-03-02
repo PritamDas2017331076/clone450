b876575c5f05fdbfb81fd9ca9d79349128d969cd
Pritam2017331076 <prishan076@gmail.com>
2023-03-02 21:43:46 +0600

    fixed a small issue

--- scripting/file3.js
+++ scripting/file3.js
@@ -1,4 +1,4 @@
-const data = require('./data');
+const data = require('./data_19');
 const axios = require('axios')
 const ip = 'http://localhost:5000'
 
@@ -6,10 +6,13 @@ const ip = 'http://localhost:5000'
 
 const func = async(obj) => {
     try {
+        console.log(obj)
         const res = await axios.post('http://localhost:5000/student/drive', obj)
-        console.log('success')
+        if (res.data.registration_number != NULL) console.log('success', res.data.registration_number)
+        else console.log('dont know', res.data, obj.registration_number)
+        console.log('')
     } catch (err) {
-        console.log('error', e)
+        console.log('error', err)
         console.log(obj)
     }
 
@@ -31,10 +34,10 @@ for (let student of data) {
         if (i == 1) obj.email = student[i]
         if (i == 2) obj.registration_number = student[i]
         if (i == 3) obj.name = student[i]
-        if (i == 6) obj.session = student[i]
-        if (i == 7) obj.phone = student[i]
-        if (i == 8) obj.file = student[i]
-        func(obj)
+        if (i == 5) obj.session = student[i]
+        if (i == 6) obj.phone = student[i]
+        if (i == 7) obj.file = student[i]
     }
+    func(obj)
 
 }
